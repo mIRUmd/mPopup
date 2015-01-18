619aa6821baac5539f291c5d878fd48745ebad94
mIRU <mIRU.md@gmail.com>
2015-01-19 00:46:25 +0100

    updated documentation

--- documentation/js/example.func.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-
-    var popup = $('#_sample1').mPopup();
-    $('#_open-sample1').on('click',function(e){
-        e.preventDefault();
-        popup.mPopup('open');
-    });
-
-});
